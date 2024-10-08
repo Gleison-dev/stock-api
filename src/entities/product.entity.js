340b85ee3c4fe1e99690ced7cb6eeef8c14fc7ea
Gleison-dev <gleisonrgomes@gmail.com>
2024-10-08 17:37:18 -0300

    feat: product entitiy created

---
+++ src/entities/product.entity.js
@@ -0,0 +1,28 @@
+import { Sequelize, DataTypes } from "sequelize";
+import { sequelize as database } from "../database/connection.js";
+
+const ProductEntity = database.define("tb_product", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  brand: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+});
+
+export { ProductEntity }
