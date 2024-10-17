56788838b3b11a373abf461e5ba544e57eb30260
Gleison Ribeiro Gomes <gleisonrgomes@gmail.com>
2024-10-17 18:54:49 -0300

    feat: adding cors on server

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ import express from "express";
 import { testConnection } from "./database/connection.js";
 import { router } from "./routes/index.route.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.SV_SERVER;
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
